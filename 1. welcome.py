d15016cfae70a4422173ece7a190c819ff92dd8a
yadavranveerp@gmail.com <yadavranveerp@gmail.com>
2022-11-26 20:54:56 +0530

    pythonBasic | Welcome message

--- 1. welcome.py
+++ 1. welcome.py
@@ -1,1 +1,14 @@
+# welcome message
 print("Welcome to python Ranveer")
+
+# print a variable value
+name = "Ranveer"
+print("My name is", name)
+
+# variable value flow
+a = 100
+print("Value of a =", a)
+a = 500
+print("Value of a =", a)
+b = a
+print("Value of b =", b)
