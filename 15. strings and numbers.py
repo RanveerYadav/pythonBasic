bfe29daeee5530f148c6eceaa8c0fb2141cb1ac0
yadavranveerp@gmail.com <yadavranveerp@gmail.com>
2022-11-28 00:31:20 +0530

    pythonBasic | Strings and Numbers

---
+++ 15. strings and numbers.py
@@ -0,0 +1,50 @@
+# Numbers
+# Example 1
+num1 = 2
+# List functions available for the object
+print(dir(num1))
+print(type(num1))
+
+# Example 2
+num2 = 2.5
+print(type(num2))
+
+# Example 3
+bool = True
+print(type(bool))
+
+# Strings
+# Example 1
+name = 'Ranveer'
+print(len(name))
+
+# Example 2
+print(name[0])
+
+# Example 3
+print(name[3])
+
+# Example 4
+print(name[-1])
+
+# Example 5
+print(name[1:3])
+
+# Example 6
+print(name[3:])
+
+# Example 7
+print(name[:4])
+
+# Example 8
+# commented because it output error and furthur is is not executed
+#print(name[8])
+
+# Example 9
+# Concatenation
+fullName = name + ' Yadav'
+print(fullName)
+
+# Repitition
+repeatName = name * 2
+print(repeatName)
