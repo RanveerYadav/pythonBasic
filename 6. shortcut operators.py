b6d0efaf82e257eaa62332af6eadee30a3b4a6de
yadavranveerp@gmail.com <yadavranveerp@gmail.com>
2022-11-27 16:34:40 +0530

    pythonBasic | Introduction

---
+++ 6. shortcut operators.py
@@ -0,0 +1,26 @@
+num1 = 10
+num2 = 3
+
+# num1 = num1 + num2
+num1 += num2
+print(num1)
+
+# num1 = num1 - num2
+num1 -= num2
+print(num1)
+
+# num1 = num1 * num2
+num1 *= num2
+print(num1)
+
+# num1 = num1 / num2
+num1 /= num2
+print(num1)
+
+# num1 = num1 // num2
+num1 //= num2
+print(num1)
+
+# num1 = num1 ** num2
+num1 **= num2
+print(num1)
